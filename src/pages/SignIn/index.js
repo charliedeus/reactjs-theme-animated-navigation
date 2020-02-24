1c5557f1c24516acc14ca1fb4be7630dd0717d91
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-02-24 14:02:11 -0300

    First commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>SignIn Page</h1>
+    </div>
+  );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from '../pages/Home';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/signin" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/profile" exact component={Profile} />
+    </Switch>
+  );
+}
